4325a06aced3d16e47bfb7612d492806a623fb51
LexKisby <kisbyalexander@gmail.com>
2020-12-22 17:20:35 +0000

    quik update

--- Parallel/test.cpp
+++ Parallel/test.cpp
@@ -9,145 +9,161 @@
 
 #include <cmath>
 
-
-
 double maxV = 0;
 double minDx = 100;
-double** x;
-double** v;
+double **x;
+double **v;
 double timeStepSize = 0.01;
-double* mass;
+double *mass;
 double t = 0;
 
 int NumberOfBodies;
-double C = 0.01/3;
+//hard coded here
+double C = 0.01 / 3;
 
-double calcDist(double* Pa, double* Pb) {
+double calcDist(double *Pa, double *Pb)
+{
     double distance = sqrt(
         (Pa[0] - Pb[0]) * (Pa[0] - Pb[0]) +
         (Pa[1] - Pb[1]) * (Pa[1] - Pb[1]) +
-        (Pa[2] - Pb[2]) * (Pa[2] - Pb[2])  
-    );
+        (Pa[2] - Pb[2]) * (Pa[2] - Pb[2]));
     return distance;
 }
 
-void calcForce(int N,int M,  double* Pa, double* Pb, double* fx, double* fy, double* fz) {
+void calcForce(int N, int M, double *Pa, double *Pb, double *fx, double *fy, double *fz)
+{
     //calculate the actual distance between N and M
     double distance = calcDist(Pa, Pb);
     minDx = std::min(minDx, distance);
     //calculate the force components along x, y, z axis.
-    *fx = (Pa[0] - Pb[0]) * mass[N] * mass[M] / distance / distance / distance; 
+    *fx = (Pa[0] - Pb[0]) * mass[N] * mass[M] / distance / distance / distance;
     *fy = (Pa[1] - Pb[1]) * mass[N] * mass[M] / distance / distance / distance;
     *fz = (Pa[2] - Pb[2]) * mass[N] * mass[M] / distance / distance / distance;
-    
 
-
-	double dis = Pa[0];
-	printf("%d %d Hello, %f \n", N, M, dis);
+    double dis = Pa[0];
+    printf("%d %d Hello, %f \n", N, M, dis);
 }
 
-void calcVel(int n, int m, double* vx, double* vy, double* vz) {
+void calcVel(int n, int m, double *vx, double *vy, double *vz)
+{
     //calc v
-    *vx = (mass[n] * v[n][0])/(mass[n]+mass[m]) + (mass[m]*v[m][0]/(mass[n]+mass[m]));
-    *vy = (mass[n] * v[n][1])/(mass[n]+mass[m]) + (mass[m]*v[m][1]/(mass[n]+mass[m]));
-    *vz = (mass[n] * v[n][2])/(mass[n]+mass[m]) + (mass[m]*v[m][2]/(mass[n]+mass[m]));
+    *vx = (mass[n] * v[n][0]) / (mass[n] + mass[m]) + (mass[m] * v[m][0] / (mass[n] + mass[m]));
+    *vy = (mass[n] * v[n][1]) / (mass[n] + mass[m]) + (mass[m] * v[m][1] / (mass[n] + mass[m]));
+    *vz = (mass[n] * v[n][2]) / (mass[n] + mass[m]) + (mass[m] * v[m][2] / (mass[n] + mass[m]));
 }
 
+void updateBody()
+{
+    maxV = 0.0;
+    minDx = std::numeric_limits<double>::max();
+    double maxMass = 0;
+    for (int m = 0; m < NumberOfBodies; m++)
+    {
+        maxMass = std::max(maxMass, mass[m]);
+    }
 
+    // forcex = force along x direction
+    // forcey = force along y direction
+    // forcez = force along z direction
+    //generate pointers to lists so each body has a force
+    double *forcex = new double[NumberOfBodies];
+    double *forcey = new double[NumberOfBodies];
+    double *forcez = new double[NumberOfBodies];
 
+    //All forces initialised to 0 for all bodies
+    for (int i = 0; i < NumberOfBodies; i++)
+    {
+        forcex[i] = 0.0;
+        forcey[i] = 0.0;
+        forcez[i] = 0.0;
+    }
 
-
-
-void updateBody() {
-  maxV   = 0.0;
-  minDx  = std::numeric_limits<double>::max();
-  double maxMass = 0;
-  for (int m=0; m<NumberOfBodies;m++) {
-      maxMass = std::max(maxMass, mass[m]);
-  }
-
-  // forcex = force along x direction
-  // forcey = force along y direction
-  // forcez = force along z direction
-  //generate pointers to lists so each body has a force
-  double* forcex = new double[NumberOfBodies];
-  double* forcey = new double[NumberOfBodies];
-  double* forcez = new double[NumberOfBodies];
-
-  //All forces initialised to 0
-  for (int i=0; i<NumberOfBodies; i++) {
-  forcex[i] = 0.0;
-  forcey[i] = 0.0;
-  forcez[i] = 0.0;
-  }
-//Iterate through all pairs of bodies and use symmetry to assign force to both bodies
-    for (int n=0; n<NumberOfBodies-1; n++) {
-        for (int m=n+1; m<NumberOfBodies; m++) {
+    //Iterate through all pairs of bodies and use symmetry to assign force to both bodies
+    //calculates the force based on current position, then will update position based on previously calculted velocity
+    for (int n = 0; n < NumberOfBodies - 1; n++)
+    {
+        for (int m = n + 1; m < NumberOfBodies; m++)
+        {
             double Gx, Gy, Gz;
             //send to calc forces and minDx
-            calcForce(n,m, x[n], x[m], &Gx, &Gy, &Gz);
+            calcForce(n, m, x[n], x[m], &Gx, &Gy, &Gz);
 
             //Add forces to pair, but negate from the other.
-            forcex[n] -= Gx; forcex[m] += Gx;
-            forcey[n] -= Gy; forcey[m] += Gy;
-            forcez[n] -= Gz; forcez[m] += Gz;
+            //force has direction so getting the right way is important
+            forcex[n] -= Gx;
+            forcex[m] += Gx;
+            forcey[n] -= Gy;
+            forcey[m] += Gy;
+            forcez[n] -= Gz;
+            forcez[m] += Gz;
 
-            printf("force: %f %f\n", forcex[n], Gx); 
-
+            printf("force: %f %f\n", forcex[n], Gx);
         }
     }
 
-//Iterate thru all and update particle pos based on prev. velocity, then compute new velocities
-    for (int i=0; i<NumberOfBodies; i++) {
+    //Iterate thru all and update particle pos based on prev. velocity, then compute new velocities to be applied next time step
+    for (int i = 0; i < NumberOfBodies; i++)
+    {
         double inc = timeStepSize * v[i][0];
-        printf("%d start: %f, increment: %f, vel: %f,  ",i, x[i][0], inc, v[i][0]);
+        printf("%d start: %f, increment: %f, vel: %f,  ", i, x[i][0], inc, v[i][0]);
         x[i][0] += timeStepSize * v[i][0];
         x[i][1] += timeStepSize * v[i][1];
         x[i][2] += timeStepSize * v[i][2];
+        //calc new velocities
         v[i][0] += timeStepSize * forcex[i] / mass[i];
         v[i][1] += timeStepSize * forcey[i] / mass[i];
         v[i][2] += timeStepSize * forcez[i] / mass[i];
 
-        
         printf("end: %f, ", x[i][0]);
         printf("speed: %f\n", v[i][0]);
     }
-//check for collisions, reduce number of bodies, maintain integrity of arrays.
-if (minDx < C*2*maxMass) {
-    for (int n=0; n<NumberOfBodies-1; n++) {
-        for (int m=n+1; m<NumberOfBodies; m++) {
-            double distance = calcDist(x[n], x[m]);
-            //check distance is less than C times mass
-            if (distance <= C * (mass[m]+mass[n])) {
-                //collide
-                double vx, vy, vz;
-                calcVel(n, m, &vx, &vy, &vz);
-                //smaller index becomes the aggregate, then final element in list becomes second element
-                v[n][0] = vx; v[n][1] = vy; v[n][2] = vz;
-                mass[n] = mass[m] + mass[n];
-                x[n][0] = (x[n][0] + x[m][0]) /2;
-                x[n][1] = (x[n][1] + x[m][1]) /2;
-                x[n][2] = (x[n][2] + x[m][2]) /2;
-                //re allocate within memory, move final body into 'm' position
-                v[m] = v[NumberOfBodies];
-                mass[m] = mass[NumberOfBodies];
-                x[m] = x[NumberOfBodies];
-                //reduce Number of Bodies by 1
-                NumberOfBodies -= 1;
+
+    //check for collisions, reduce number of bodies, maintain integrity of arrays.
+    //basic bottleneck test, from minDx calculated in the force calcualtions
+    //if the min distance is greater than the constraint for the largest mass item in the system , then no collisions have occurred.
+    if (minDx <= C * 2 * maxMass)
+    {
+        //otherwise check every item
+        for (int n = 0; n < NumberOfBodies - 1; n++)
+        {
+            for (int m = n + 1; m < NumberOfBodies; m++)
+            {
+                double distance = calcDist(x[n], x[m]);
+                //check distance is less than C times mass
+                if (distance <= C * (mass[m] + mass[n]))
+                {
+                    //collide
+                    double vx, vy, vz;
+                    calcVel(n, m, &vx, &vy, &vz);
+                    //smaller index becomes the aggregate, then final element in list becomes second element
+                    v[n][0] = vx;
+                    v[n][1] = vy;
+                    v[n][2] = vz;
+                    //sum mass
+                    mass[n] = mass[m] + mass[n];
+                    //avg position //need to weight the avg!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+                    x[n][0] = (x[n][0] + x[m][0]) / 2;
+                    x[n][1] = (x[n][1] + x[m][1]) / 2;
+                    x[n][2] = (x[n][2] + x[m][2]) / 2;
+                    //re allocate within memory, move final body into 'm' position
+                    v[m] = v[NumberOfBodies - 1];
+                    mass[m] = mass[NumberOfBodies - 1];
+                    x[m] = x[NumberOfBodies - 1];
+                    //reduce Number of Bodies by 1 so
+                    NumberOfBodies -= 1;
+                }
             }
         }
     }
-}
 
-
-
-//ending adjustment
-maxV = 0;
-double tempV = 0;
-for (int i=0; i<NumberOfBodies; i++) {
-    tempV += v[i][0] * v[i][0] + v[i][1] * v[i][1] + v[i][2] * v[i][2];
-    maxV = std::max(std::sqrt(tempV), maxV);
-}
+    //ending adjustment to find max velocity
+    maxV = 0;
+    double tempV = 0;
+    for (int i = 0; i < NumberOfBodies; i++)
+    {
+        tempV += v[i][0] * v[i][0] + v[i][1] * v[i][1] + v[i][2] * v[i][2];
+        maxV = std::max(std::sqrt(tempV), maxV);
+    }
     t += timeStepSize;
     printf("time:  %f,   \n", t);
 
@@ -157,42 +173,39 @@ for (int i=0; i<NumberOfBodies; i++) {
     printf("maxV: %f,  minDx: %f \n", maxV, minDx);
 }
 
+int main()
+{
 
-
-
-
-
-int main() {
-    
     int T = 5;
     NumberOfBodies = 3;
-    x    = new double*[NumberOfBodies];
-    v    = new double*[NumberOfBodies];
-    mass = new double [NumberOfBodies];
+    x = new double *[NumberOfBodies];
+    v = new double *[NumberOfBodies];
+    mass = new double[NumberOfBodies];
 
-    for (int i=0; i<NumberOfBodies; i++) {
-    x[i] = new double[3];
-    v[i] = new double[3];
-    mass[i] = 5;
-        for (int j=0; j<3; j++) {
+    for (int i = 0; i < NumberOfBodies; i++)
+    {
+        x[i] = new double[3];
+        v[i] = new double[3];
+        mass[i] = 5;
+        for (int j = 0; j < 3; j++)
+        {
             v[i][j] = 0;
         }
     }
-//set locations
+    //set locations
     x[0][0] = 10;
-	x[0][1] = 0;
-	x[0][2] = 0;
+    x[0][1] = 0;
+    x[0][2] = 0;
     x[1][0] = 11;
-	x[1][1] = 0;
-	x[1][2] = 0;
-	x[2][0] = 0;
+    x[1][1] = 0;
+    x[1][2] = 0;
+    x[2][0] = 0;
     x[2][1] = 50;
-	x[2][2] = 0;
+    x[2][2] = 0;
 
-
-
-    while (t< T) {
+    while (t < T)
+    {
         updateBody();
-	printf("Byeeee\n\n");
+        printf("Byeeee\n\n");
     }
 }
